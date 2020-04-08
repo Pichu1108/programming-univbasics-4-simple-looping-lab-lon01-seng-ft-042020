a3e4cd38dcca2dba6fd301768bc54a70638fe92f
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-08 15:28:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,14 @@
-# Write your methods here
+def loop_message_five_times(message)
+  counter = 0
+  while counter <= 5
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times (message, number)
+  counter = 0
+  while counter <= number
+    puts message
+  end
+end
