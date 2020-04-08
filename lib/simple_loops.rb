87027b9bce8a83016827d06e2aaa4cd81db2772d
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-08 15:49:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,6 @@
 def loop_message_five_times(message)
   counter = 0
-  while counter <= 5
+  while counter <= 5 do
     puts message
     counter += 1
   end
@@ -8,7 +8,7 @@ end
 
 def loop_message_n_times (message, number)
   counter = 0
-  while counter < number
+  while counter < number do
     puts message
     counter += 1
   end
@@ -20,7 +20,7 @@ end
 
 def return_string_array(array)
   counter = 0
-  while counter <= array.length
+  while counter <= array.length do
     array[counter].to_s
     counter += 1
   end
