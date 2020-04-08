3d3069fd5b0cdc0d3bfeaa2685bdd6211414889a
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-08 15:51:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,10 +19,9 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  counter = 0
-  while counter <= array.length do
-    array[counter].to_s
-    counter += 1
+  count = 0
+  while count < array.length do
+    array[count].to_s
+    count += 1
   end
-  array
 end
